49854c09991c197e351c412ecbd4be73c69a0b52
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-05-30 12:17:37 -0700

    add simple component

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
-console.log('hello world')
+import React from 'react'
+import { render } from 'react-dom'
+import Title from './components/Title'
+
+render(<Title name = "roma" />, document.getElementById('container'))
