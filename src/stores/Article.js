77dd6670823ef38c08bc03d0040bce5ca30209c4
Roman Iakobchuk <r.iakobchuk@gmail.com>
2016-06-13 12:27:51 -0700

    add articles to store

--- src/stores/Article.js
+++ src/stores/Article.js
@@ -1,11 +1,11 @@
 import BasicStore from './BasicStore'
-import { DELETE_ARTICLE, ADD_COMMENT } from '../constants'
+import { DELETE_ARTICLE, ADD_COMMENT, LOAD_ALL_ARTICLES_START, LOAD_ALL_ARTICLES_SUCCESS, LOAD_ALL_ARTICLES_FAIL } from '../constants'
 
 export default class ArticleStore extends BasicStore {
     constructor(...args) {
         super(...args)
         this._subscribe((action) => {
-            const { type, payload } = action
+            const { type, payload, response, error } = action
 
             switch (type) {
                 case DELETE_ARTICLE:
@@ -16,6 +16,21 @@ export default class ArticleStore extends BasicStore {
                     this._waitFor(['comments'])
                     const article = this.getById(payload.articleId)
                     article.comments = (article.comments || []).concat(payload.comment.id)
+                    break
+
+                case LOAD_ALL_ARTICLES_START:
+                    this.loading = true
+                    break
+
+                case LOAD_ALL_ARTICLES_SUCCESS:
+                    response.forEach(this._add)
+                    this.loading = false
+                    break
+
+                case LOAD_ALL_ARTICLES_FAIL:
+                    this.error = error
+                    this.loading = false
+                    break
 
                 default:
                     return
